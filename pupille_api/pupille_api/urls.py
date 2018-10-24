28031993bb51d116a983d0a32b762796bdc57daf
Ivette <car16020@uvg.edu.gt>
2018-10-23 21:05:50 -0600

    user authentication

--- pupille_api/pupille_api/urls.py
+++ pupille_api/pupille_api/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+# It’s always a good idea to version the APIs 
+# We´ll set the route as api/v1/ for our api app.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pupille/v1/', include('pupille.urls')),
 ]
