9d57cffbf6312605abe21b12a4c1e0dcd1066e7a
Ivette <car16020@uvg.edu.gt>
2018-10-23 23:16:37 -0600

    new models

--- pupille_api/pupille/admin.py
+++ pupille_api/pupille/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subject, Theme, Card, Comment
+
+admin.site.register(Subject)
+admin.site.register(Theme)
+admin.site.register(Card)
+admin.site.register(Comment)
--- pupille_api/pupille/models.py
+++ pupille_api/pupille/models.py
@@ -1,6 +1,37 @@
 from django.db import models
+from users.models import CustomUser
  
+class Subject(models.Model):
+    subject_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, unique=False, blank=False, null=False)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, related_name='subjects', on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.name
+
+class Theme(models.Model):
+    themes_id = models.IntegerField(primary_key=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=False, blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    commentary = models.ManyToManyField(CustomUser, through= 'Comment', related_name='comment')
+
+class Card(models.Model):
+    card_id = models.IntegerField(primary_key=True)
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
+    question = models.CharField(max_length=100, blank=False, null=False)
+    answer = models.CharField(max_length=100, blank=False, null=False)
 
 
-
+class Comment(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='created_by', on_delete=models.CASCADE)
+    theme = models.ForeignKey(Theme, related_name='theme', on_delete=models.CASCADE)
+    content = models.TextField(blank=False, null=False)
+    creation_datetime = models.DateTimeField(auto_now=True)
 
